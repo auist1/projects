60aaee7c18b2252c7abae68c5ff4d6c1b03267be
Ahmet Ali Ugurlu <95145076+auist1@users.noreply.github.com>
2024-12-25 14:11:08 +0100

    Bikestore_analysis.sql

--- Bikestore/Bikestore_analysis.sql
+++ Bikestore/Bikestore_analysis.sql
@@ -25,12 +25,16 @@ SELECT COUNT(DISTINCT city) AS NumberofCities FROM Customers
 
 -- What is the total revenue year by year?
 
+CREATE VIEW orderitems AS
+SELECT order_id, item_id, product_id,quantity,list_price,discount, quantity*list_price*(1-discount) AS totalrevenue
+FROM Order_items
+
 SELECT 
-	YEAR(order_date) AS Years, ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRev
+	YEAR(order_date) AS Years, SUM(TotalRevenue) AS TotalRev
 FROM 
 	Orders o
 LEFT JOIN 
-	Order_items oi ON o.order_id=oi.order_id
+	orderitems oi ON o.order_id=oi.order_id
 GROUP BY 
 	YEAR(order_date)
 
@@ -47,13 +51,13 @@ GROUP BY
 -- Show total revenue, total amount of products sold and yearly unit prices in the same table. 
 SELECT
 	YEAR(order_date) AS Years,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRev,
+	SUM(totalrevenue) AS TotalRev,
 	SUM(oi.quantity) AS TotalQuantity,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount))/SUM(oi.quantity),0) AS UnitPrice
+	ROUND(SUM(totalrevenue)/SUM(oi.quantity),0) AS UnitPrice
 FROM 
 	Orders o
 JOIN 
-	Order_items oi ON o.order_id=oi.order_id
+	orderitems oi ON o.order_id=oi.order_id
 GROUP BY 
 	YEAR(order_date)
 
@@ -61,17 +65,17 @@ GROUP BY
 WITH TR AS (
 	SELECT 
 		s.store_name, 
-		ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRev
+		ROUND(SUM(totalrevenue),0) AS TotalRev
 	FROM Stores s
 	LEFT JOIN Orders o ON s.store_id=o.store_id
-	LEFT JOIN Order_items oi ON oi.order_id=o.order_id
+	LEFT JOIN orderitems oi ON oi.order_id=o.order_id
 	GROUP BY s.store_name
 ),
 TR2 AS (
-	SELECT ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TTR
+	SELECT ROUND(SUM(totalrevenue),0) AS TTR
 	FROM Stores s
 	LEFT JOIN Orders o ON s.store_id=o.store_id
-	LEFT JOIN Order_items oi ON oi.order_id=o.order_id
+	LEFT JOIN orderitems oi ON oi.order_id=o.order_id
 )
 SELECT
 TR.store_name,
@@ -85,16 +89,16 @@ TR
 WITH TC AS (
 	SELECT 
 		c.category_name,
-		ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRev
+		ROUND(SUM(totalrevenue),0) AS TotalRev
 	FROM Categories c
 	LEFT JOIN Products p ON p.category_id=c.category_id
-	LEFT JOIN Order_items oi ON oi.product_id=p.product_id
+	LEFT JOIN orderitems oi ON oi.product_id=p.product_id
 	GROUP BY c.category_name
 ),
 TTC AS (
 	SELECT 
-		ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TTotalRev
-	FROM Order_items oi
+		ROUND(SUM(totalrevenue),0) AS TTotalRev
+	FROM orderitems oi
 )
 SELECT 
 	TC.category_name,
@@ -124,11 +128,11 @@ ORDER BY
 SELECT 
 	c.state,
 	SUM(oi.quantity) AS Qunatity,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRev,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount))/SUM(oi.quantity),0) AS UnitPrice
+	SUM(totalrevenue) AS TotalRev,
+	ROUND(SUM(totalrevenue)/SUM(oi.quantity),0) AS UnitPrice
 FROM Customers c
 LEFT JOIN Orders o ON c.customer_id=o.customer_id
-LEFT JOIN Order_items oi ON oi.order_id=o.order_id
+LEFT JOIN orderitems oi ON oi.order_id=o.order_id
 GROUP BY c.state
 
 -- What are the minimum, maximum and average list prices per categories? 
@@ -155,9 +159,9 @@ GROUP BY
 
 SELECT 
 	YEAR(o.order_date) Years,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)) / COUNT (DISTINCT o.order_id),0) AS BasketSize
+	ROUND(SUM(totalrevenue) / COUNT (DISTINCT o.order_id),0) AS BasketSize
 FROM
-	Order_items oi 
+	orderitems oi 
 LEFT JOIN 
 	Orders o 
 ON 
@@ -168,14 +172,14 @@ GROUP BY
 -- List Top 10 Products according to their sales revenue and list their minimum, average and maximum prices. 
 SELECT TOP 10
 	p.product_name,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRevenue,
-	ROUND(MIN(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS Minimum,
-	ROUND(AVG(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS Average,
-	ROUND(MAX(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS Maximum
+	SUM(totalrevenue) AS TotalRevenue,
+	MIN(totalrevenue) AS Minimum,
+	AVG(totalrevenue) AS Average,
+	MAX(totalrevenue) AS Maximum
 FROM
 	Products p
 LEFT JOIN 
-	Order_items oi 
+	orderitems oi 
 ON 
 	oi.product_id=p.product_id
 GROUP BY 
@@ -190,7 +194,7 @@ SELECT
 	c.city,
 	p.product_name,
 	ca.category_name,
-	ROUND(SUM(oi.quantity * oi.list_price * (1-oi.Discount)),0) AS TotalRevenue
+	ROUND(SUM(totalrevenue),0) AS TotalRevenue
 FROM 
 	Customers c
 LEFT JOIN 
@@ -198,7 +202,7 @@ LEFT JOIN
 ON 
 	o.customer_id=c.customer_id
 LEFT JOIN 
-	Order_items oi
+	orderitems oi
 ON
 	oi.order_id=o.order_id
 LEFT JOIN 
@@ -278,13 +282,13 @@ ON
 
 SELECT 
     p.product_name,
-    AVG(CASE WHEN YEAR(o.order_date) = 2016 THEN oi.quantity * oi.list_price * (1 - oi.discount) ELSE NULL END) AS 'Revenue_2016',
-    AVG(CASE WHEN YEAR(o.order_date) = 2017 THEN oi.quantity * oi.list_price * (1 - oi.discount) ELSE NULL END) AS 'Revenue_2017',
-    AVG(CASE WHEN YEAR(o.order_date) = 2018 THEN oi.quantity * oi.list_price * (1 - oi.discount) ELSE NULL END) AS 'Revenue_2018'
+    AVG(CASE WHEN YEAR(o.order_date) = 2016 THEN totalrevenue ELSE NULL END) AS 'Revenue_2016',
+    AVG(CASE WHEN YEAR(o.order_date) = 2017 THEN totalrevenue ELSE NULL END) AS 'Revenue_2017',
+    AVG(CASE WHEN YEAR(o.order_date) = 2018 THEN totalrevenue ELSE NULL END) AS 'Revenue_2018'
 FROM
     Products p
 LEFT JOIN 
-    Order_items oi
+    orderitems oi
 ON
     oi.product_id = p.product_id
 LEFT JOIN
